7bdd1274a16cc336448d96a700ab6d341b13012f
troymcfarland99 <115182370+troymcfarland99@users.noreply.github.com>
2022-11-02 16:14:55 -0400

    Add files via upload
    added files

---
+++ singaporetroy.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+projectFolder=~/Documents/rohr_inf
+
+cat rawsing.csv | gcut -d, -f3-5 --complement rawsing.csv > trimsing.csv
+
+rm formatsing.csv
+for i in {3..44}
+do
+
+diseasename=$(head -1 trimsing.csv | cut -d, -f$i)
+cat trimsing.csv | sed 1d | cut -d, -f1,2,$i | awk -v dname="$diseasename" -F"," '{ OFS = "," } {$4=dname; print}' >> formatsing.csv
+
+done
+
